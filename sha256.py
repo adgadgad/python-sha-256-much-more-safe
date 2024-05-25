e5fa9a387631e3d48a44307d137c5e635e954449
adgadgad <137925334+adgadgad@users.noreply.github.com>
2024-05-24 23:27:39 -0600

    Update sha256.py

--- sha256.py
+++ sha256.py
@@ -151,5 +151,5 @@ def _rotate_right(num: int, shift: int, size: int = 32):
 
 if __name__ == "__main__":
     print(generate_hash("Hello").hex())
-    print(generate_hash("Hello").hex())
+    
     
